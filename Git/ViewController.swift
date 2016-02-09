1e9d674e694c54ae92e5a118394b77444d8728a3
Rodrigo Busto <Centurion@rodrigos-imac.local>
2016-02-08 21:48:41 -0600

    Initial Commit

---
+++ Git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by Rodrigo Busto on 2/8/16.
+//  Copyright © 2016 Rodrigo Busto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
